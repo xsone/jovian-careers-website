42e428aadc9512ad7e657dd57a0f0e0e09819540
xsone <xsone2@gmail.com>
2024-06-14 17:28:25 +0200

    Initial commit Updated Flask project v2.0

--- app.py
+++ app.py
@@ -2,12 +2,33 @@
 
 
 from flask import Flask, render_template
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def plot():
+    #return render_template('home.html')
+    left = [1, 2, 3, 4, 5]
+    # heights of bars
+    height = [10, 24, 36, 40, 5]
+    # labels for bars
+    tick_label = ['one', 'two', 'three', 'four', 'five']
+    # plotting a bar chart
+    plt.bar(left, height, tick_label=tick_label, width=0.8, color=['red', 'green'])
+
+    # # naming the y-axis
+    # plt.ylabel('y - axis')
+    # # naming the x-axis
+    # plt.xlabel('x - axis')
+    # # plot title
+    # plt.title('My bar chart!')
+    #
+    # plt.savefig('static/images/plot.png')
+
+    return render_template('home.html', url='/static/CVboiler.png')
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
